7213df340b47ecc888ac4f4a2f28eb46dfe9342d
LazyLZ <laizhi727@126.com>
2017-03-09 23:03:08 +0800

    First Commit Test

---
+++ default.js
@@ -0,0 +1,29 @@
+/**
+ * Created by LZ on 2017/1/30.
+ */
+button = document.getElementById('button');
+/*
+var info = document.getElementsByClassName('window-info');
+info[0].innerHTML = window.screen.height;
+info[1].innerHTML = window.innerHeight;
+info[2].innerHTML = window.screen.availHeight;
+info[3].innerHTML = window.outerHeight;
+
+var message = window.prompt("?","goot");
+    alert(message);
+
+alert('back!');
+window.history.back();
+alert('sorry,go');
+window.history.forward();
+button = document.getElementById('button');
+button.onclick = function(){
+    timeID = window.setInterval(function(){
+        window.open('https://www.baidu.com');
+    },100);
+    setTimeout(function () {
+        clearInterval(timeID);
+    },1000);
+};
+*/
+
